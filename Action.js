19577873ee151fcde4d8b9f2829ba64f0c03c468
unknown <OnchariWycliffe@AzureAD>
2019-04-08 19:15:59 +0300

    This is my first commit

---
+++ Action.js
@@ -0,0 +1,32 @@
+//"'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+//" Script Title       : 
+//"                      
+//" Script Date        : Mon Apr  8 19:15:33 2019
+//"                       
+//"'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+function Action()
+{
+	web.setSocketsOption('SSL_VERSION', 'TLS1.2');
+
+	web.addHeader('Upgrade-Insecure-Requests', 
+		'1');
+
+	web.url(
+		{
+			name : 'blazedemo.com', 
+			url : 'http://blazedemo.com/', 
+			resource : 0, 
+			recContentType : 'text/html', 
+			referer : '', 
+			snapshot : 't1.inf', 
+			mode : 'HTML', 
+			extraRes :  [
+				{url : '/favicon.ico'}
+			]
+		}
+	);
+
+	return 0;
+}
+
